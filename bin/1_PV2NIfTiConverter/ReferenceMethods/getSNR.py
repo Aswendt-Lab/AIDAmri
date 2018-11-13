91ad6b215806d255501f6e440dde10ddd9f1ec67
pallastn <niklas.pallast@uk-koeln.de>
2018-11-13 09:14:34 +0100

    Optimize SNR calculation

--- bin/1_PV2NIfTiConverter/ReferenceMethods/getSNR.py
+++ bin/1_PV2NIfTiConverter/ReferenceMethods/getSNR.py
@@ -10,62 +10,59 @@ University Hospital Cologne
 
 """
 
-import os
+import os, sys
 import changSNR as ch
 import brummerSNR as bm
 import sijbersSNR as sj
 import numpy as np
 import glob
 import nibabel as nii
-def snrCalclualtor(input_file):
-    fileSNR = open(os.path.join(os.path.dirname(input_file),'snr.txt'), 'w')
 
+
+def snrCalclualtor(input_file, method):
+    fileSNR = open(os.path.join(os.path.dirname(input_file),'snr.txt'), 'w')
 
     data = nii.load(input_file)
     imgData = data.get_data()
-
-
 
     #nx = imgData.shape[0] # Images size in x - direction
     #ny = imgData.shape[1] # Images size in y - direction
     ns = imgData.shape[2] # Number of slices
 
-    noiseChSNR = np.zeros(ns)
-    noiseBmSNR = np.zeros(ns)
-    noiseSjSNR = np.zeros(ns)
+    noiseSNR = np.zeros(ns)
 
     imgData = np.ndarray.astype(imgData,'float64')
     for slc in range(ns):
         #   Print % of progress
         print('Slice: ' + str(slc + 1))
 
+
         # Temporal image containing all TE values for the selected slice
         slice = imgData[:, :, slc]
 
-        curSnrCHMap, estStdChang, estStdChangNorm = ch.calcSNR(slice, 0, 1)
+        if method == 1:
+            curSnr, estStd, estStdNorm = ch.calcSNR(slice, 0, 1)
 
-        curSnrBMMap, estStdBrummer, estStdBrummerNorm = bm.calcSNR(slice, 0, 1)
+        elif method == 2:
+            curSnr, estStd, estStdNorm = bm.calcSNR(slice, 0, 1)
 
-        curSnrSJMap, estStdSijbers, estStdSijbersNorm = sj.calcSNR(slice, 0, 1)
+        elif method == 3:
+            curSnr, estStd, estStdNorm = sj.calcSNR(slice, 0, 1)
+        else:
+            sys.exit(
+                "Error: '%i' is not an existing choice for a SNR method!" % (method,))
 
-        noiseChSNR[slc] = estStdChang
-        noiseBmSNR[slc] = estStdBrummer
-        noiseSjSNR[slc] = estStdSijbers
+        noiseSNR[slc] = estStd
 
+    snr = 20 * np.log10(np.mean(imgData) / np.mean(noiseSNR))
 
-    snrCh = 20*np.log10(np.mean(imgData)/np.mean(noiseChSNR))
-    snrBrum = 20*np.log10(np.mean(imgData)/np.mean(noiseBmSNR))
-    snrSij = 20*np.log10(np.mean(imgData)/np.mean(noiseSjSNR))
-
-    fileSNR.write("Mean of Chang: %0.3f \n" %snrCh )
-    fileSNR.write("Mean of Brummer: %0.3f \n" %snrBrum )
-    fileSNR.write("Mean of Sijbers: %0.3f \n" %snrSij )
+    fileSNR.write("SNR [dB]: %0.3f \n" % snr)
     fileSNR.close()
 
 
 def findRegisteredData(path):
     regMR_list = []
-    for filename in glob.iglob(path+'/P5/SS*/T2w/*1.nii.gz', recursive=True):
+    for filename in glob.iglob(path + '*/T2w/*1.nii.gz', recursive=True):
         regMR_list.append(filename)
 
     return regMR_list
@@ -73,17 +70,19 @@ def findRegisteredData(path):
 if __name__ == "__main__":
     import argparse
 
-    parser = argparse.ArgumentParser(description='Calculate SNR')
-    parser.add_argument('-p','--pathData', help='file name:Brain extracted input data')
+    parser = argparse.ArgumentParser(description='Calculates SNR and generates snr.txt in T2w files')
+    parser.add_argument('-p', '--pathData', help='src path to all processed files')
+    parser.add_argument('-f', '--filePrefix', help='file prefix in src path')
+    parser.add_argument('-m', '--SNRmethod', help='1: Brummer(default) 2:Chang 3:Sijbers', nargs='?', type=int,
+                        default=1)
 
     args = parser.parse_args()
 
-    pathData = args.pathData
+    pathData = args.pathData + '/' + args.filePrefix
 
     listMr = findRegisteredData(pathData)
+    method = args.SNRmethod
 
     for i in listMr:
         print(i)
-        snrCalclualtor(i)
-
-
+        snrCalclualtor(i, method)
