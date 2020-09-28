b703a368c55218d8103b918cf6c5b6d2b093517b
Leon Scharwächter <leon.scharwaechter@gmail.com>
2020-09-28 17:54:12 +0200

    Delete old version

--- ARA/readXML_Lables.m
+++
@@ -1,8 +0,0 @@
-function annotation50CHANGEDannolabelIDs = readXML_Lables(xml_file)
-%% Import the data
-[~, ~, annotation50CHANGEDannolabelIDs] = xlsread(xml_file);
-annotation50CHANGEDannolabelIDs = annotation50CHANGEDannolabelIDs(2:end,5);
-
-annotation50CHANGEDannolabelIDs = string(annotation50CHANGEDannolabelIDs);
-annotation50CHANGEDannolabelIDs(ismissing(annotation50CHANGEDannolabelIDs)) = '';
-end
