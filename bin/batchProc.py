9d3047edc02e82bbf4d4f203ac5a2c5f66b74558
Marc Schneider <marc@schneider-lein.de>
2023-10-27 13:15:34 +0200

    Renaming to fit brkraw output

--- bin/batchProc.py
+++ bin/batchProc.py
@@ -18,17 +18,19 @@ import glob
 import os
 import fnmatch
 
-def findData(projectPath, days, groups):
-    # This function screens all existing paths based on the specified days
-    # and groups. Within these paths, this function collects all subject
+def findData(projectPath):
+    # This function screens all existing paths. Within these paths, this function collects all subject
     # folders, which are all folders that are not named 'Physio'.
-    fullPath_list = []
-    path_list = [os.path.join(projectPath, days[d], groups[g]) for d in range(len(days)) for g in range(len(groups))]
-    for checkPath in path_list:
-        list_subfolders = [f.name for f in os.scandir(checkPath) if f.is_dir() and f.name.lower() != 'physio']
-        for subject in list_subfolders:
-            fullPath_list.append(os.path.join(checkPath, subject))
-    return(fullPath_list)
+    full_path_list = []
+    full_path_list = os.listdir(projectPath)
+    all_wanted_paths = []
+    for path in full_path_list:  
+        if "sub" in path and not ".DS_Store" in path:
+            wanted_paths = os.listdir(os.path.join(projectPath, path))
+            wanted_paths = [os.path.join(projectPath, path, wanted_path) for wanted_path in wanted_paths if "ses" in wanted_path]
+            all_wanted_paths.extend(wanted_paths)
+
+    return all_wanted_paths
 
 def executeScripts(fullPath, dataTypeInput, stc, *optargs):
     # For every datatype (T2w, fMRI, DTI), go in all days/group/subjects folders
@@ -41,19 +43,19 @@ def executeScripts(fullPath, dataTypeInput, stc, *optargs):
     for dataFormat in dataTypeInput:
         for currentPath in fullPath:
             currentPath_wData = os.path.join(currentPath,dataFormat)
-            # currentPath_wData = projectfolder/day/group/subject/dataFormat (e.g. T2w, fMRI, DTI)
+            # currentPath_wData = projectfolder/sub/ses/dataFormat (e.g. anat, func, dwi)
             if os.path.isdir(currentPath_wData):
-                if dataFormat == 'T2w':
+                if dataFormat == 'anat':
                     os.chdir(cwd + '/2.1_T2PreProcessing')
-                    currentFile = find("*1.nii.gz", currentPath_wData)
+                    currentFile = find("*T2w.nii.gz", currentPath_wData)
                     if len(currentFile)>0:
                         print('Run python 2.1_T2PreProcessing/preProcessing_T2.py -i '+currentFile[0])
                         os.system('python preProcessing_T2.py -i '+currentFile[0])
                     else:
-                        message = 'Could not find *1.nii.gz in '+currentPath_wData;
+                        message = 'Could not find *T2w.nii.gz in '+currentPath_wData;
                         print(message)
                         errorList.append(message)
-                    currentFile = find("*BiasBet.nii.gz", currentPath_wData)
+                    currentFile = find("*Bet.nii.gz", currentPath_wData)
                     if len(currentFile)>0:
                         print('Run python 2.1_T2PreProcessing/registration_T2.py -i '+currentFile[0])
                         os.system('python registration_T2.py -i '+currentFile[0])
@@ -67,14 +69,14 @@ def executeScripts(fullPath, dataTypeInput, stc, *optargs):
                     print('Run python 3.1_T2Processing/getIncidenceSize.py -i '+currentPath_wData)
                     os.system('python getIncidenceSize.py -i '+currentPath_wData)
                     os.chdir(cwd)
-                elif dataFormat == 'fMRI':
+                elif dataFormat == 'func':
                     os.chdir(cwd + '/2.3_fMRIPreProcessing')
-                    currentFile = find("*1.nii.gz", currentPath_wData)
+                    currentFile = find("*EPI.nii.gz", currentPath_wData)
                     if len(currentFile)>0:
                         print('Run python 2.3_fMRIPreProcessing/preProcessing_fMRI.py -i '+currentFile[0])
                         os.system('python preProcessing_fMRI.py -i '+currentFile[0])
                     else:
-                        message = 'Could not find *1.nii.gz in '+currentPath_wData;
+                        message = 'Could not find *EPI.nii.gz in '+currentPath_wData;
                         print(message)
                         errorList.append(message)
                     currentFile = find("*SmoothBet.nii.gz", currentPath_wData)
@@ -91,14 +93,14 @@ def executeScripts(fullPath, dataTypeInput, stc, *optargs):
                         os.chdir(cwd + '/3.3_fMRIActivity')
                         os.system('python process_fMRI.py -i '+ currentFile[0] + ' -stc ' + str(stc))
                     os.chdir(cwd)
-                elif dataFormat == 'DTI':
+                elif dataFormat == 'dwi':
                     os.chdir(cwd + '/2.2_DTIPreProcessing')
-                    currentFile = find("*1.nii.gz", currentPath_wData)
+                    currentFile = find("*dwi.nii.gz", currentPath_wData)
                     if len(currentFile)>0:
                         print('Run python 2.2_DTIPreProcessing/preProcessing_DTI.py -i '+currentFile[0])
                         os.system('python preProcessing_DTI.py -i '+currentFile[0])
                     else:
-                        message = 'Could not find *1.nii.gz in '+currentPath_wData;
+                        message = 'Could not find *dwi.nii.gz in '+currentPath_wData;
                         print(message)
                         errorList.append(message)
                     currentFile = find("*SmoothMicoBet.nii.gz", currentPath_wData)
@@ -109,7 +111,7 @@ def executeScripts(fullPath, dataTypeInput, stc, *optargs):
                         message = 'Could not find *SmoothMicoBet.nii.gz in '+currentPath_wData;
                         print(message)
                         errorList.append(message)
-                    currentFile = find("*1.nii.gz", currentPath_wData)
+                    currentFile = find("*dwi.nii.gz", currentPath_wData)
 
                     # Appends optional (fa0, nii_gz) flags to DTI main process if passed
                     if len(currentFile)>0:
@@ -148,40 +150,34 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description='Batch processing of all data. This script runs every needed script for all registration and processing steps. The data needs to be ordered like after Bruker2NIfTI conversion: project_folder/days/groups/subjects/. For the script to work, it needs to be placed within the /bin folder of AIDAmri. Example: python batchProc.py -f /Volumes/Desktop/MRI/proc_data -g Treatment_C3a Treatment_PBS -d Baseline P7 P14 P28 P42 P56 -t T2w fMRI DTI')
     requiredNamed = parser.add_argument_group('required arguments')
-    requiredNamed.add_argument('-f', '--folder', required=True,
+    requiredNamed.add_argument('-i', '--input', required=True,
                         help='Path to the parent project folder of the dataset, e.g. proc_data')
-    requiredNamed.add_argument('-g', '--groups', required=True, nargs='+', help='Group names as in the Bruker2NIfTI processed project folder')
-    requiredNamed.add_argument('-d', '--days', required=True, nargs='+', help='Day names as in the Bruker2NIfTI processed project folder')
-    requiredNamed.add_argument('-t', '--dataTypes', required=True, nargs='+', help='Data types to be processed e.g. T2w, DTI and/or fMRI. Multiple specifications are possible.')
+
 
     optionalNamed = parser.add_argument_group('optional arguments')
-    optionalNamed.add_argument('-o', '--optional', nargs = '*', help = 'Optional arguments.\n\t"fa0": Renames the FA metric data to former DSI naming convention.\n\t"nii_gz": Converts ROI labeling relating files from .nii to .nii.gz format to match former data structures.')
     optionalNamed.add_argument('-stc', '--slicetimecorrection', default = "False", type=str,
                                help='Set True or False if a slice time correction should be performed. Only set true if you converted raw bruker data with conv2nifti.py from aidamri beforehand. Otherwise choose False')
+    optionalNamed.add_argument('-d', '--days', required=False, nargs='+', help='Day names as in the Bruker2NIfTI processed project folder')
+    optionalNamed.add_argument('-t', '--dataTypes', required=False, nargs='+', help='Data types to be processed e.g. anat, dwi and/or func. Multiple specifications are possible.')
 
     args = parser.parse_args()
-    pathToData = args.folder
-    groupNames = args.groups
-    dayNames = args.days
-    dataTypes = args.dataTypes
+    pathToData = args.input
+
     if args.slicetimecorrection is None:
         stc = False
     else:
         stc = args.slicetimecorrection
-    if args.optional is not None:
-        optionals = ' '.join(args.optional)
+    if args.dataTypes is None:
+        dataTypes = ["anat", "dwi", "func"]
     else:
-        optionals = args.optional
+        dataTypes = args.dataTypes
     
     print('Entered information:')
-    print(groupNames)
-    print(dayNames)
-    print(dataTypes)
-    print('Optional arguments: [%s]' % optionals)
+    print(pathToData)
     print('Slice time correction [%s]' % stc)
 
-    listMr = findData(pathToData, dayNames, groupNames)
-    if optionals is not None:
-        executeScripts(listMr, dataTypes, stc, optionals)
-    else:
-        executeScripts(listMr, dataTypes, stc)
+    listMr = findData(pathToData)
+    
+    executeScripts(listMr, dataTypes, stc)
+    
+ 
