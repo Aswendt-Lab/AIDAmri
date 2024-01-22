1d255a23fe4735649e25e823a055ac10b8374003
Markus Aswendt <markus.aswendt@uk-koeln.de>
2024-01-22 13:34:17 +0100

    added brain_cc mask for 1_Ants

--- bin/1_Ants/reg_ants_list.sh
+++ bin/1_Ants/reg_ants_list.sh
@@ -5,6 +5,7 @@ path_066_anuka="/beegfs/v1/ivnmr_group/studies/066_AnukaMarinaDREADD/processed_d
 processed_data="/daten/ivnmr_scratch/michaeld/reg_ants/processed_data"
 
 #$path_script -i ${path_066_anuka}/stroke_cells/2wk/rsfMRI_ANTs $processed_data stroke_cells 2wk AM_30674_1_4_20170820_124922 5 1 8 1
+#																				group	time point	study dirname 	t2w scan nr. reco nr. rs-fMRI nr. reco. nr
 $path_script -i ${path_066_anuka}/stroke_cells/2wk/rsfMRI_ANTs $processed_data stroke_cells 2wk AM_30675_1_3_20170820_135627 5 1 8 1
 
 #$path_script -i ${path_066_anuka}/stroke_cells/6wk/rsfMRI_ANTs $processed_data stroke_cells 6wk AM_30674_1_6_20170918_122017 5 1 8 1
