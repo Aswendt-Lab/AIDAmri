90fd13c8ad62c85a9b4f0b63d7dbbf9cf3fb91f8
Marc Schneider <marc@schneider-lein.de>
2024-01-31 13:36:07 +0100

    bug fix process functional data

--- bin/batchProc.py
+++ bin/batchProc.py
@@ -79,6 +79,8 @@ def findData(projectPath, sessions, dataTypes):
 def run_subprocess(command,datatype, step):
     command_args = shlex.split(command)
     file = command_args[-1]
+    if datatype == "func" and step =="process":
+        file = command_args[-3]
     log_file = os.path.join(os.path.dirname(file), step + ".log")
     if datatype == "anat" and step == "process":
         log_file = os.path.join(os.path.dirname(file), datatype, step + ".log")
