67493a2efcc40a0f3f27884bed18a7fe080b9896
Julian Cramer <cramer.julian96@web.de>
2025-09-01 11:05:34 +0200

    frac changed

--- bin/2.1_T2PreProcessing/preProcessing_T2.py
+++ bin/2.1_T2PreProcessing/preProcessing_T2.py
@@ -105,7 +105,7 @@ def applyBET(input_file,frac,radius,vertical_gradient, center=None):
 #default_rad  = 45
 #default_vert = 0.0
 
-default_frac = 0.38
+default_frac = 0.34
 default_rad  = 60
 default_vert = 0.08
 default_bias_skip = 0.0 #1.0 for skip 0.0 for run
