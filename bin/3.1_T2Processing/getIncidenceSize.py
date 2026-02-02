a0cbc85e1ba77fef3fe6b8216012fdf16a06c398
Julian Cramer <cramer@med.uni-frankfurt.de>
2026-02-02 19:03:43 +0100

    feat: enhance incidence size calculation in getIncidenceSize.py for improved accuracy and performance

--- bin/3.1_T2Processing/getIncidenceSize.py
+++ bin/3.1_T2Processing/getIncidenceSize.py
@@ -19,6 +19,7 @@ import scipy.ndimage as ndimage
 
 def thresholding(volumeMR,maskImg,thres,k):
     volumeMR=ndimage.gaussian_filter(volumeMR, sigma=(1.3, 1.3, 1))
+    volumeMR = volumeMR.copy()
     zvalues = volumeMR != 0
 
     if k==1:
@@ -40,23 +41,24 @@ def thresholding(volumeMR,maskImg,thres,k):
 def incidenceMap(path_listInc,path_listMR ,path_listAnno, araDataTemplate,incidenceMask ,thres, outfile, labels):
 
     araDataTemplate  = nii.load(araDataTemplate)
-    realAraImg = araDataTemplate.get_data()
-    coloredAraLabels = np.zeros([np.size(realAraImg, 0), np.size(realAraImg, 1), np.size(realAraImg, 2)])
+    realAraImg = np.asanyarray(araDataTemplate.dataobj)
+    coloredAraLabels = np.zeros(realAraImg.shape, dtype=realAraImg.dtype)
+
 
     matFile = sc.loadmat(labels)
     labMat = matFile['ABALabelIDs']
 
     maskData = nii.load(incidenceMask)
-    maskImg = maskData.get_data()
+    maskImg = np.asanyarray(maskData.dataobj).copy()
     oneValues = maskImg > 0.0
     maskImg[oneValues] = 1.0
     fileIndex = 0
 
     # get warped annos of the current mr
     dataAnno = nii.load(path_listAnno[fileIndex])
-    volumeAnno = np.round(dataAnno.get_data())
+    volumeAnno = np.round(np.asanyarray(dataAnno.dataobj))
     dataMR = nii.load(path_listInc[fileIndex])
-    volumeMR = dataMR.get_data()
+    volumeMR = np.asanyarray(dataMR.dataobj)
 
     strokeVolume = thresholding(volumeMR, maskImg, thres,1)
 
@@ -92,16 +94,18 @@ def incidenceMap(path_listInc,path_listMR ,path_listAnno, araDataTemplate,incide
 
     # Stroke volume calculation
     betMask = nii.load(os.path.join(outfile,os.path.basename(path_listInc[fileIndex]).split('.')[0]+'_mask.nii.gz'))
-    betMaskImg = betMask.get_data()
+    betMaskImg = np.asanyarray(betMask.dataobj).copy()
     oneValues = betMaskImg > 0.0
     betMaskImg[oneValues] = 1.0
-    strokeVolumeInCubicMM = np.sum(maskImg * (dataMR.affine[0, 0] * dataMR.affine[1, 1] * dataMR.affine[2, 2]))
-    brainVolumeInCubicMM = np.sum(betMaskImg * (dataMR.affine[0, 0] * dataMR.affine[1, 1] * dataMR.affine[2, 2]))
+    voxel_vol = abs(np.linalg.det(dataMR.affine[:3, :3]))
+    strokeVolumeInCubicMM = np.sum(strokeVolume > 0) * voxel_vol
+    brainVolumeInCubicMM = np.sum(betMaskImg > 0) * voxel_vol
+
+    strokePercent = (strokeVolumeInCubicMM / brainVolumeInCubicMM * 100.0) if brainVolumeInCubicMM > 0 else 0.0
 
     lines = open(os.path.abspath(os.path.join(os.getcwd(), os.pardir,os.pardir))+ '/lib/ARA_changedAnnotatiosn2DTI.txt').readlines()
     o=open(os.path.join(outfile, 'affectedRegions.txt'), 'w')
-    o.write("Stroke: %0.2f %% - Stroke Volume: %0.2f mm^3\n" % (
-    ((strokeVolumeInCubicMM / brainVolumeInCubicMM) * 100), strokeVolumeInCubicMM,))
+    o.write("Stroke: %0.2f %% - Stroke Volume: %0.2f mm^3\n" % (strokePercent, strokeVolumeInCubicMM))
     matIndex = 0
     labelNamesAffected = ["" for x in range(np.size(fValues_Anno))]
     labelNames = ["" for x in range(np.size(lines))]
@@ -118,7 +122,7 @@ def incidenceMap(path_listInc,path_listMR ,path_listAnno, araDataTemplate,incide
     matFile['ABALabelIDs'] = labMat
     matFile['ABANames'] = labelNamesAffected
     matFile['ABAlabels'] = labelNames
-    matFile['volumePer'] = (strokeVolumeInCubicMM / brainVolumeInCubicMM) * 100
+    matFile['volumePer'] = strokePercent
     matFile['volumeMM'] = strokeVolumeInCubicMM
     sc.savemat(os.path.join(outfile, 'labelCount.mat'), matFile)
 
@@ -211,4 +215,4 @@ if __name__ == "__main__":
     if not len(regANNO_list) == len(regMR_list):
         sys.exit("Error: For one or more annotations no corresponding MR file is defined in '%s'." % (inputFolder,))
 
-    incidenceMap(regMR_list,regInc_list,regANNO_list,araDataTemplate,incidenceMask,thres,outfile,labels)
+    incidenceMap(regInc_list, regMR_list,regANNO_list,araDataTemplate,incidenceMask,thres,outfile,labels)
