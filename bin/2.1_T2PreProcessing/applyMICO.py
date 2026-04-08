2e3793e85664697fd2ad6369db01a188c4c483c9
Julian Cramer <cramer@med.uni-frankfurt.de>
2026-04-08 12:29:10 +0200

    feat: ensure NIfTI image data is stored as float32 in bias correction output

--- bin/2.1_T2PreProcessing/applyMICO.py
+++ bin/2.1_T2PreProcessing/applyMICO.py
@@ -179,8 +179,9 @@ def run_MICO(IMGdata,outputPath):
 
     progressbar.close()
 
-    unscaledNiiData = nii.Nifti1Image(biasCorrectedVol, data.affine)
+    unscaledNiiData = nii.Nifti1Image(biasCorrectedVol.astype(np.float32), data.affine)
     hdrOut = unscaledNiiData.header
+    hdrOut.set_data_dtype(np.float32)
     hdrOut.set_xyzt_units('mm')
 
     outputData = os.path.join(outputPath,os.path.basename(IMGdata).split('.')[0]+'Bias.nii.gz')
