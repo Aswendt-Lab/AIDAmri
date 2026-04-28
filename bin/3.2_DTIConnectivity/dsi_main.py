40f5f55c3b99c011a7782b73bb53811fa81e0103
Julian Cramer <cramer@med.uni-frankfurt.de>
2026-04-28 17:18:00 +0200

    fix: update command line argument for skipping motion correction in DSI-Studio

--- bin/3.2_DTIConnectivity/dsi_main.py
+++ bin/3.2_DTIConnectivity/dsi_main.py
@@ -133,8 +133,7 @@ if __name__ == '__main__':
                         help='Legacy file types for DSI-Studio releases before 2024. Default is False (uses new more storage-efficient ".sz" and ".fz" file types)',
                         action = 'store_true'
                         )
-    parser.add_argument('-nomcf',
-                        '--no_motion_correction',
+    parser.add_argument('--no_motion_correction',
                         action='store_true',
                         help='Specify whether to skip motion correction. Default is False (perform motion correction). Set to "true" to skip motion correction.',
                         required=False
